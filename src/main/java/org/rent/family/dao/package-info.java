39b640f28dad9a1df72df942714c10faa125686c
吴雨佳 <wuyujia@beiquan.org>
2017-05-24 23:02:08 +0800

    包结构创建和Domain类编写

---
+++ src/main/java/org/rent/family/dao/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Copyright © 2017北京联合倍全电子商务有限公司. All rights reserved.
+ *
+ * @Prject: family-record
+ * @Package: org.rent.family.dao
+ * @Description: TODO
+ * @author: wuyujia
+ * @Date: 2017/5/24 22:49
+ * @version: V1.0
+ */
+package org.rent.family.dao;
